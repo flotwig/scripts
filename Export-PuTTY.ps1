cd68d7ed9d51c588ef8015a809365a55711e1c3d
Zach Bloomquist <zrbq@live.com>
2016-03-02 16:08:42 -0500

    Adding PuTTY Export

---
+++ Export-PuTTY.ps1
@@ -0,0 +1,6 @@
+﻿<#
+.SYNOPSIS
+One-liner to export PuTTY settings from the registry to OneDrive.
+#>
+$onedrive = (Get-ItemProperty -Path "hkcu:\Software\Microsoft\OneDrive\" -Name UserFolder).UserFolder
+regedit /e "$onedrive\putty.reg" HKEY_CURRENT_USER\Software\SimonTatham
