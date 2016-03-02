af926d72602f39a208fc0cc9a03cf9569e35f274
Zach Bloomquist <zrbq@live.com>
2016-03-02 16:10:55 -0500

    Fixing synopsis

--- Import-PuTTY.ps1
+++ Import-PuTTY.ps1
@@ -1,6 +1,6 @@
 ﻿<#
 .SYNOPSIS
-One-liner to import PuTTY settings from the registry to OneDrive.
+One-liner to import PuTTY settings from OneDrive to the registry.
 #>
 $onedrive = (Get-ItemProperty -Path "hkcu:\Software\Microsoft\OneDrive\" -Name UserFolder).UserFolder
 regedit /e "$onedrive\putty.reg" HKEY_CURRENT_USER\Software\SimonTatham
